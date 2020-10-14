cfe7428b6b50bbea82682c77c4056fd48b5bb7f2
Frank Carr <gundamtrooperx@gmail.com>
2020-10-14 16:00:58 -0700

    added componentdidmount to employee.js

--- wk19-assignment/src/pages/Employee.js
+++ wk19-assignment/src/pages/Employee.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import Axios from 'axios';
 
-class Employee extends React.Component{
-    state={
-        employeerecords:[]
+class Employee extends React.Component {
+    state = {
+        employeerecords: []
     }
-    componentDidMount=()>{
-
+    componentDidMount = () => {
+        var employeerecords = [];
+        Axios.get('https://randomuser.me/api/?results=200&nat=us').then((response)=>{
+            console.log(response)
+            for(let i = 0; i < response.data.results.length; i++){
+                employeerecords.push({
+                    id: response.data.results[i].login.uuid,
+                    fname: response.data.results[i].name.first ,
+                    lname: response.data.results[i].name.last ,
+                    cellphone: response.data.results[i].cell ,
+                    location: response.data.results[i].location
+                })
+            }
+            this.setState({employeerecords})
+            console.log(this.state.employeerecords)
+        })
     }
     render() {
-        return(<div className='container'>
+        return (<div className='container'>
             hiiiii
         </div>)
-        
+
     }
 }
 
