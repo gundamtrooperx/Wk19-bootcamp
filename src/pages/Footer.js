4d1de076a77db7f71407027c2ff7ad76467d031b
Frank Carr <gundamtrooperx@gmail.com>
2020-11-04 16:11:07 -0700

    Updates

--- src/pages/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-function Footer () {
-    return (<div className='jumbotron'>
-        <h1>Copy Rights Frank &copy;</h1>
-    </div>)
-}
-
-export default Footer;
--- src/pages/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-function Header () {
-    return (<div className='jumbotron'>
-        <h1>Employee User Directory</h1>
-    </div>)
-}
-
-export default Header;
