7d6df1416a20be6c7ea687965ffa6e5024d91197
Frank Carr <gundamtrooperx@gmail.com>
2020-11-04 15:46:04 -0700

    Gh-pages deploy trial1

--- 19assignment/src/pages/Employee.js
+++ 19assignment/src/pages/Employee.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
 import EmpolyeeDetail from "./EmployeeDetail";
-
+//REACT Class component
 
 class Employee extends React.Component {
     state = {
